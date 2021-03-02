4ee18ee88f29a6551b3ff1bf6a6fb56180f90ebc
Aliexpress228 <59725483+Aliexpress228@users.noreply.github.com>
2021-03-02 22:30:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+jQuery('document').ready(function(){
+    jQuery('button').on('click',function(){
+        jQuery('.heading').append('<h1>Если появился этот текст то на гитхабе работает javascript</h1>');
+    });
+});
