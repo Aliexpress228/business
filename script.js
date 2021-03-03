9bec3cdd5191319bbeec42f878396881d4389205
Aliexpress228 <59725483+Aliexpress228@users.noreply.github.com>
2021-03-03 22:05:03 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-jQuery('document').ready(function(){
-    jQuery('button').on('click',function(){
-        jQuery('.heading').append('<h1>Если появился этот текст то на гитхабе работает javascript</h1>');
-    });
-});
