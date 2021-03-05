3a6cc2caaf24d608a852272d1407faa2cdb40212
Aliexpress228 <59725483+Aliexpress228@users.noreply.github.com>
2021-03-05 21:45:55 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-jQuery('document').ready(function(){
-    jQuery('.teamate').on('click',function(){
-        jQuery('#team').append('<h4 style="color:red;">Извините. Но клуб МОЛОДЫХ И ПЕРСПЕКТИВНЫХ открыт только для МОЛОДЫХ И ПЕРСПЕКТИВНЫХ</h4>');
-    });
-});
